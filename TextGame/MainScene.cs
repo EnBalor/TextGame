75cfa1e094807fb330e934f6a85c1cc0dcef978f
EnBalor <enbalor4334@gmail.com>
2024-04-25 16:17:29 +0900

    MainSceneFix

--- TextGame/MainScene.cs
+++ TextGame/MainScene.cs
@@ -4,14 +4,10 @@ namespace TextGame
 {
     internal class MainScene
     {
-        public static List<object> itemList = new List<object>();
-
         static void Main(string[] args)
         {
             StartScene startScene = new StartScene();
             startScene.Print();
         }
-
-        
     }
 }
